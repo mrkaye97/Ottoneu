2212c93f44b0ae9635454d2e3dd04d8bb3f4884a
Matt Kaye <mrkaye97@gmail.com>
2020-08-03 18:33:34 -0400

    Update PointProj.py

--- daily-projections/PointProj.py
+++ daily-projections/PointProj.py
@@ -168,3 +168,6 @@ with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.sendmail(
         sender_email, receiver_email, message.as_string()
     )
+
+
+driver.quit()
